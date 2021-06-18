3aa3f58a9a66d374545bc111f5151009d276f715
Maxim <konstantinovmax@gmail.com>
2021-06-18 16:11:21 +0300

    feat: реализовать функционал drag-n-drop

---
+++ README.md
@@ -0,0 +1,5 @@
+# Drag-and-drop
+
+В рамках тренировочного проекта реализована технология drag-n-drop.
+
+**[Ссылка на демо](https://konstantinovmax.github.io/drag-and-drop/)**
---
+++ script.js
@@ -0,0 +1,39 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+placeholders.forEach((item) => {
+    item.addEventListener('dragover', dragover);
+    item.addEventListener('dragenter', dragenter);
+    item.addEventListener('dragleave', dragleave);
+    item.addEventListener('drop', dragdrop);
+});
+
+function dragstart(evt) {
+    evt.target.classList.add('hold');
+    setTimeout(() => evt.target.classList.add('hide'), 0);
+}
+
+function dragend(evt) {
+    evt.target.classList.remove('hold');
+    evt.target.classList.remove('hide');
+}
+
+function dragover(evt) {
+    evt.preventDefault();
+}
+
+function dragenter(evt) {
+    evt.target.classList.add('hovered');
+}
+
+function dragleave(evt) {
+    evt.target.classList.remove('hovered');
+}
+
+function dragdrop(evt) {
+    evt.target.append(item);
+    evt.target.classList.remove('hovered');
+}
